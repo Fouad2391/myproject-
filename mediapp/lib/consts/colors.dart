66fb0f3a497819a790d1cc8f264d728971ebe6f1
Fouad Jallous <fuad.elcellus@gmail.com>
2024-07-21 14:51:10 +0300

    creat mediapp

---
+++ mediapp/lib/consts/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff2A7FBA);
+  static const Color yellowColor = Color(0xffFF9F00);
+  static const Color bgColor = Color(0xffF5F5F5);
+  static const Color bgDarkColor = Color(0xffECECEC);
+}
---
+++ mediapp/lib/consts/strings.dart
@@ -0,0 +1,18 @@
+class AppStrings {
+  static String appname = "ميدي آب",
+      bestDocApp = "أفضل تطبيق لمواعيد الأطباء",
+      welcomeBack = "مرحبًا بعودتك!",
+      weAreExcited = "نحن متحمسون لعودتك!",
+      signupNow = "سجل الآن وابدأ في استكشاف كل ما يقدمه تطبيقنا.",
+      email = "البريد الإلكتروني",
+      emailHint = "أدخل بريدك الإلكتروني...",
+      password = "كلمة المرور",
+      passwordHint = "أدخل كلمة المرور هنا...",
+      fullName = "الاسم الكامل",
+      fullNameHint = "أدخل اسمك هنا...",
+      confirmPassword = "تأكيد كلمة المرور",
+      login = "تسجيل الدخول",
+      signup = "تسجيل",
+      alreadyHaveAccount = "هل لديك حساب؟",
+      dontHaveAccount = "ليس لديك حساب؟";
+}
